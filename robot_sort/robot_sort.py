87faed8abfd69a2c3390e4b943a7f3810021e506
PetejStone <petejstoney@gmail.com>
2019-09-13 10:23:12 -0700

    finished sorting

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -1,13 +1,35 @@
+# It can move left or right.
+# It can pick up an item
+# If it tries to pick up an item while already holding one, it will swap the items instead.
+# It can compare the item it's holding to the item in front of it.
+# It can switch a light on its head on or off.
+
+
+# You may use any pre-defined robot methods.
+# You may NOT modify any pre-defined robot methods.
+# You may use logical operators. (if, and, or, not, etc.)
+# You may use comparison operators. (>, >=, <, <=, ==, is, etc.)
+# You may use iterators. (while, for, break, continue)
+# You may NOT store any variables. (=)
+# You may NOT access any instance variables directly. (self._anything)
+# You may NOT use any Python libraries or class methods. (sorted(), etc.)
+# You may define robot helper methods, as long as they follow all the rules.
+
+
+
+
 class SortingRobot:
-    def __init__(self, l):
+    def __init__(self, arr):
         """
         SortingRobot takes a list and sorts it.
         """
-        self._list = l          # The list the robot is tasked with sorting
+        self._list = arr          # The list the robot is tasked with sorting
         self._item = None       # The item the robot is holding
         self._position = 0      # The list position the robot is at
         self._light = "OFF"     # The state of the robot's light
         self._time = 0          # A time counter (stretch)
+
+        
 
     def can_move_right(self):
         """
@@ -92,21 +114,49 @@ class SortingRobot:
         """
         return self._light == "ON"
 
+    #################### Plan for Sorting ############
+       # SWAP 
+        ### Iterative swap example - loops through values, if one is greater, swap values
+        ##### if current index was 2      .
+        ##                          [ 1,2,4,3,5 ]
+        ## inside loop found that 4 is > 3, so new cur_index is 3 (3) -- > cur_index = arr[3]
+        ### temp = arr[2] (4) - so temp value is 4
+        #### arr[2] == arr[3] -- so 4 is now = 3 -> [1,2,3,3,5]
+        #### arr[cur_index] = temp   --> aka  -->  (arr[3]) == arr[2] -> [1,2,3,4,5]
+
+      ##################################################
+
     def sort(self):
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        
+        for i in range(0, len(self._list) - 1):
+            cur_index = i  
+            smallest_index = cur_index
+           
+    
+            #Loop through array and if the current index > then any of the items, set the cur index to that item
+            #Loop through i + 1 -- the next item through the length of the array
+            for j in range(i+1, len(self._list)):
+                if self._list[cur_index] > self._list[j]:
+                    cur_index = j
+            ####
+            temp = self._list[i] #temp variable is == the current position in the outside loop
+        
+            self._list[i] = self._list[cur_index] #current position of outside loop is set equal to the new current index from the inside loop
+        
+            self._list[cur_index] = temp #current index is set equal to the temp variable
+        return self._list
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    arr = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
-    robot = SortingRobot(l)
+    robot = SortingRobot(arr)
 
     robot.sort()
     print(robot._list)
